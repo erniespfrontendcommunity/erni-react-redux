1513571d434e739f9cac43338c52b0461990da63
Konstantin Kalbazov <kalbazov@gmail.com>
2017-03-24 20:16:55 +0100

    Support enter key for add todo field

--- src/js/events.js
+++ src/js/events.js
@@ -10,6 +10,13 @@ export function registerEventHandlers() {
     event.stopPropagation();
   });
 
+  listen('keyup', '#todoInput', event => {
+    if (event.keyCode == 13) {
+      todos.dispatch(addTodo(event.target.value));
+      event.target.value = '';
+    }
+  });
+
   listen('click', '.js_toggle_todo', event => {
     const id = Number.parseInt(event.target.getAttribute('data-id'), 10);
     todos.dispatch(toggleTodoState(id));
